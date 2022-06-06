a035249894b4c4bef53ab164a2dd458bd42c4190
Matti Tahvonen <matti@vaadin.com>
2022-06-06 14:48:30 +0300

    Directory packaging and V23 upgrade
    breaks some icons (+/- in NumberField & add clear button for datefields), probably differences that should be fixed for V23.

--- src/test/java/com/example/application/Application.java
+++ src/test/java/com/example/application/Application.java
@@ -1,6 +1,10 @@
 package com.example.application;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -12,7 +16,10 @@ import org.vaadin.artur.helpers.LaunchUtil;
 @SpringBootApplication
 @NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
 @NpmPackage(value = "line-awesome", version = "1.3.0")
-public class Application extends SpringBootServletInitializer {
+@PWA(name = "Fiber", shortName = "Fiber")
+@Theme("fiber")
+@PageTitle("Fiber")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
         LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
