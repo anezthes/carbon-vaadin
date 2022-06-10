86e022ade1776f4d6a6690ab606c6c7a541fcd44
Joacim Päivärinne <joacim@vaadin.com>
2022-06-10 14:50:57 +0300

    Packages re-added; apparently needed for some reason.

---
+++ frontend/generated/theme.js
@@ -0,0 +1,2 @@
+import {applyTheme as _applyTheme} from './theme-carbon.generated.js';
+export const applyTheme = _applyTheme;
--- src/test/java/com/example/application/Application.java
+++ src/test/java/com/example/application/Application.java
@@ -1,5 +1,6 @@
 package com.example.application;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.theme.Theme;
@@ -12,6 +13,8 @@ import org.vaadin.artur.helpers.LaunchUtil;
  * The entry point of the Spring Boot application.
  */
 @SpringBootApplication
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
 @Theme("carbon")
 @PageTitle("Carbon for Vaadin")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
