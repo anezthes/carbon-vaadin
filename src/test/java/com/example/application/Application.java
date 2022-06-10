07969266f5a0dffbc105b6655d194d008b77b986
Joacim Päivärinne <joacim@vaadin.com>
2022-06-10 18:15:54 +0300

    Icon fix

--- src/test/java/com/example/application/Application.java
+++ src/test/java/com/example/application/Application.java
@@ -13,6 +13,7 @@ import org.vaadin.artur.helpers.LaunchUtil;
  * The entry point of the Spring Boot application.
  */
 @SpringBootApplication
+@NpmPackage(value = "line-awesome", version = "1.3.0")
 @Theme("carbon")
 @PageTitle("Carbon for Vaadin")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
